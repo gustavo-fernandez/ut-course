98fd692508e2b2e16031892b1a1eaf7427e1afa7
Gustavo Fernandez <fersangustavo@gmail.com>
2021-04-25 17:50:15 -0500

    Calculadora full testing

--- src/main/java/com/peru/Calculadora.java
+++ src/main/java/com/peru/Calculadora.java
@@ -2,8 +2,27 @@ package com.peru;
 
 public class Calculadora {
 
+  public static int count = 0;
+
   public int sumar(int a, int b) {
     return a + b;
+  }
+
+  public Integer convertAInteger(int a) {
+    return Integer.valueOf(a);
+  }
+
+  public int[] obtenerDivisionYResto(int dividendo, int divisor) {
+    int division = dividendo / divisor;
+    int resto = dividendo % divisor;
+    return new int[] { division, resto };
+  }
+
+  public int dividir(int dividendo, int divisor) throws Exception {
+    if (divisor == 0) {
+      throw new Exception("divisor no puede ser 0");
+    }
+    return dividendo / divisor;
   }
 
 }
--- src/test/java/com/peru/CalculadoraTest.java
+++ src/test/java/com/peru/CalculadoraTest.java
@@ -1,21 +1,86 @@
 package com.peru;
 
-import org.junit.Test;
+import org.junit.*;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class CalculadoraTest {
 
+  private Calculadora calc;
+
+  @BeforeClass
+  public static void beforeAll() {
+    System.out.println(">> Antes de cualquier test se ejecute");
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    System.out.println(">> Después de que todos se ejecutaron");
+  }
+
+  @Before
+  public void beforeXYZ() {
+    System.out.println("Antes de cada test");
+    calc = new Calculadora();
+  }
+
+  @After
+  public void after() {
+    Calculadora.count = 0;
+    System.out.println("después de cada test");
+  }
+
   @Test
   public void testSuma() {
+    System.out.println("Ejecutando test suma");
     // given (establacemos variables, clases, objetos necesarios para el test)
-    Calculadora calc = new Calculadora();
     int sumando1 = 14;
     int sumando2 = 10;
     // when (Ejecutamos el código o método que queremos testear)
     int resultado = calc.sumar(sumando1, sumando2);
     // then (Verificamos el código que hemos ejecutado, esto es que cumpla con el resultado esperado)
-    assertTrue(resultado == 24);
+    // assertTrue(resultado == 24);
+    assertEquals(24, resultado);
+  }
+
+  @Test
+  public void probarConversionAWrapper() {
+    // given
+    int intPrimitivo = 100;
+    // when
+    Integer integerWrapper = calc.convertAInteger(intPrimitivo);
+    // then
+    assertEquals(Integer.valueOf(100), integerWrapper);
+    assertNotNull(integerWrapper);
+    assertFalse(integerWrapper == null);
+  }
+
+  @Test
+  public void probarDivision() {
+    System.out.println("Ejecutando test division");
+    // given
+    int dividendo = 9;
+    int divisor = 2;
+    // when
+    int[] resultadoDivision =
+        calc.obtenerDivisionYResto(dividendo, divisor);
+    // then
+    int[] resultadoEsperado = {4, 1};
+    assertArrayEquals(resultadoEsperado, resultadoDivision);
+  }
+
+  @Test
+  public void validarDivision() throws Exception {
+    int dividendo = 9;
+    int divisor = 4;
+    int resDivision = calc.dividir(dividendo, divisor);
+
+    assertEquals(2, resDivision);
+  }
+
+  @Test(expected = Exception.class)
+  public void divisionEntreCero() throws Exception {
+    calc.dividir(10, 0);
   }
 
 }
